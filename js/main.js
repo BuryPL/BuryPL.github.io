7eb0cd565ac9792e65040611392b3a7cb27651e9
Krzysztof Burkot <buryPL@gmail.com>
2017-04-23 16:45:22 +0200

    Bootstrap_smoothscroll+hiding_navbar

--- js/main.js
+++ js/main.js
@@ -9,3 +9,31 @@ $(document).ready(function () {
     });
 });
 
+/*ŻEBY POBRAĆ WHEELDELTA TRZEBA UŻYĆ .BIND('MOUSEWHEEL',...*/
+$(window).bind('mousewheel', function (e) {
+    var position = $(document).scrollTop();
+    var menu_bar = $('#menu');
+    if (position < 200) {
+        menu_bar.css('background-color', 'rgba(255,255,255,1)');
+        menu_bar.css('margin-top', '0');
+        console.log('scrollowanie niżej 200');
+    } else {
+        menu_bar.css('background-color', 'rgba(255,255,255,0)');
+        console.log(menu_bar.css('margin-top'));
+        if (e.originalEvent.wheelDelta == -120) {
+            if (parseInt(menu_bar.css('margin-top')) == 0) {
+                menu_bar.animate({
+                    marginTop: '-50',
+                }, 1000, function () {
+                    console.log('animacja chowania zakończona')
+                });
+            } else {
+                console.log('schowane');
+            }
+        } else {
+            menu_bar.animate({
+                marginTop: '0',
+            }, 500);
+        }
+    }
+});
